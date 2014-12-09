e5b8d331ea9597fc6a2648debef3e5dc4574bd25
bahakz <bahakz@gmail.com>
2014-12-10 02:23:17 +0600

    test file added

---
+++ GitTest/TestFile.h
@@ -0,0 +1,13 @@
+//
+//  TestFile.h
+//  GitTest
+//
+//  Created by Bakytzhan Baizhikenov on 12/10/14.
+//  Copyright (c) 2014 intellection. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestFile : NSObject
+
+@end
---
+++ GitTest/TestFile.m
@@ -0,0 +1,13 @@
+//
+//  TestFile.m
+//  GitTest
+//
+//  Created by Bakytzhan Baizhikenov on 12/10/14.
+//  Copyright (c) 2014 intellection. All rights reserved.
+//
+
+#import "TestFile.h"
+
+@implementation TestFile
+
+@end
